3dcd01c51c536a79c607e27ce730ded3b9aebe5b
Malgorzata Kot <malgorzata.kot@jobcloud.ch>
2020-08-18 12:26:23 +0200

    webpack set up

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,27 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require("path");
+const express = require("express");
+const mockAPIResponse = require("./mockAPI.js");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const app = express()
+console.log(`Your API key is ${process.env.API_KEY}`);
 
-app.use(express.static('dist'))
+const app = express();
 
-console.log(__dirname)
+app.use(express.static("dist"));
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+console.log(__dirname);
+
+app.get("/", function (req, res) {
+  res.sendFile("dist/index.html");
+  // res.sendFile(path.resolve("src/client/views/index.html"));
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+  console.log("Example app listening on port 8080!");
+});
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.get("/test", function (req, res) {
+  res.send(mockAPIResponse);
+});
